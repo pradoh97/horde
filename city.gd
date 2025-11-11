ba734f18b55678e1f45a0164fb44dda26209f5bc
pradoh97 <pradoh97@gmail.com>
2025-11-11 17:54:23 -0300

    Add capturable city

---
+++ city.gd
@@ -0,0 +1,43 @@
+class_name City extends Area2D
+
+@export var troops_to_capture: int = 5
+@export var max_spawn_speed: float = 1500
+@export var min_spawn_speed: float = 700
+@export var tween_duration: float = 1
+var captured = false
+var troops_in_city: int = 0
+
+func _ready():
+	update_troops_count_label()
+
+func update_troops_count_label():
+	$Label.text = str(troops_to_capture - troops_in_city)
+
+func capture_city():
+	captured = true
+	$TroopSpawnTimer.start()
+	var tween: Tween = create_tween()
+	tween.set_parallel()
+	tween.tween_property($Polygon2D, "color", Color("#3f6ecc"), tween_duration)
+	tween.tween_property($Polygon2D2, "color", Color("#e1cd28"), tween_duration)
+
+func spawn_troop():
+	var new_minion: Minion = Minion.new_minion()
+	new_minion.global_position = global_position
+	get_parent().add_child(new_minion)
+	new_minion.velocity = Vector2(randf_range(-max_spawn_speed, max_spawn_speed), randf_range(-max_spawn_speed, max_spawn_speed))
+
+func _on_body_entered(minion: Minion):
+	if not captured:
+		if minion.army and not minion.is_leading:
+			troops_in_city += 1
+			minion.kill()
+			update_troops_count_label()
+		if troops_to_capture - troops_in_city == 0:
+			capture_city()
+
+
+
+func _on_troop_spawn_timer_timeout():
+	spawn_troop()
+	$TroopSpawnTimer.start()
