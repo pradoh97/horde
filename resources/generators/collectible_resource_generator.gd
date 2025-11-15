394ff54b2cf4debe77996adbaf9acb4baf94b607
pradoh97 <pradoh97@gmail.com>
2025-11-15 11:47:17 -0300

    Move files and rename

--- resources/generators/collectible_resource_generator.gd
+++ resources/generators/collectible_resource_generator.gd
@@ -1,7 +1,9 @@
-class_name CollectibleResource extends Area2D
+class_name CollectibleResourceGenerator extends Area2D
 
 @export var max_resources: int = 5
+@export var generation_factor: float = 1.0
 @export var use_first_free_minion: bool = false
+@export var resource_generated: CollectibleResource = null
 var resources_available: int = 0
 
 func _ready():
@@ -10,16 +12,13 @@ func _ready():
 func update_count_label():
 	$Label.text = str(resources_available)
 
-func get_image() -> Sprite2D:
-	return $Sprite2D
-
 func _on_body_entered(minion: Minion):
 	if minion.army and resources_available > 0:
-		if minion.pick_resource(self):
+		if minion.pick_resource(resource_generated):
 			resources_available -= 1
 			update_count_label()
 
 func _on_level_day_passed():
 	if resources_available < max_resources:
-		resources_available += 1
+		resources_available += floor(1*generation_factor)
 		update_count_label()
