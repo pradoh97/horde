f7a320c375936a42bb46fb716899cb5bd719708c
pradoh97 <pradoh97@gmail.com>
2025-11-21 16:31:37 -0300

    Prevent pick up of weapons before activating

--- resources/generators/weapon_rack.gd
+++ resources/generators/weapon_rack.gd
@@ -9,6 +9,6 @@ func _on_level_day_passed():
 	pass
 
 func _on_body_entered(minion: Minion):
-	if minion.army and $Fare.payment_valid(minion):
+	if minion.army and $Fare.payment_valid(minion) and enabled:
 		if minion.pick_up_collectible(collectible_generated):
 			$Fare.charge_payment(minion)
