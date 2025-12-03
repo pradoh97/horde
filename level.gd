f0044bccee9cefca338f343026fd2079dd39874a
pradoh97 <pradoh97@gmail.com>
2025-12-03 11:36:12 -0300

    Create single player and pvp level

--- level.gd
+++ level.gd
@@ -4,11 +4,11 @@ signal day_passed
 @export var day_duration: int = 3
 
 func _ready():
-	$Mouse.area_entered.connect($Army._on_mouse_area_entered)
-	$Mouse.area_exited.connect($Army._on_mouse_area_exited)
+	if get_node_or_null("Mouse"):
+		$Mouse.area_entered.connect($Army._on_mouse_area_entered)
+		$Mouse.area_exited.connect($Army._on_mouse_area_exited)
 	$DayTicker.wait_time = day_duration
 	$DayTicker.start()
-	$Army.level = self
 
 func _on_day_ticker_timeout():
 	day_passed.emit()
