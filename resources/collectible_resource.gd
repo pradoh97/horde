9dad085f6c64e779f3b3e5ead84dc70a77513f4f
pradoh97 <pradoh97@gmail.com>
2025-12-09 19:00:03 -0300

    change type for enum

--- resources/collectible_resource.gd
+++ resources/collectible_resource.gd
@@ -1,4 +1,5 @@
 class_name CollectibleResource extends Resource
 
-@export var type: String = ""
+enum TYPE {WOOD=0, STONE=1, FOOD=2, WEAPON=3}
+@export var type: TYPE
 @export var texture: Texture2D = null
